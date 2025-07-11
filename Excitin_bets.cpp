6505d1451e057c994ab299490473a8288f0e6030
Arya <arypatel913@gmail.com>
2025-07-11 09:51:58 -0700

    subsequence

--- Excitin_bets.cpp
+++ Excitin_bets.cpp
@@ -1,21 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-
-int main() {
+int main()
+{
     int t;
-    cin >> t;
-    while(t--) {
-        int a, b;
-        cin >> a >> b;
-
-        if (a == b) {
-            cout << 0 << " " << 0 << endl;
-        } else {
-            int d = abs(a - b);
-            int moves = min(a % d, d - a % d);
-            cout << d << " " << moves << endl;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        vector<long long>v(n);
+        long long count1=0;
+        long long count0=0;
+        for(long long i=0;i<n;i++){
+            cin>>v[i];
         }
+        for(long long i=0;i<n;i++){
+            if(v[i]==1){
+                count1++;
+            }
+            else if(v[i]==0){
+                count0++;
+            }
+        }
+      long long ways=pow(2,count0)*count1;
+      cout<<ways<<endl;
     }
-    return 0;
 }
