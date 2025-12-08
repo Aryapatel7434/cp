d0c977bc7f02322849def6fc0e754bc9d1dfc8bd
Arya <arypatel913@gmail.com>
2025-12-08 19:07:58 +0530

    java

---
+++ funwithsubarr.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class funwithsubarr{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+
+        int t=sc.nextInt();
+
+        while(t-- > 0){
+             int n=sc.nextInt();
+
+             int[]v=new int[n];
+
+             for(int i=0;i<n;i++){
+                v[i]=sc.nextInt();
+             }
+
+             int ans=0;
+             int i=n-1;
+
+             while(i>=0 && v[i]==v[n-1]){
+                i--;
+             }
+
+             if(i==-1){
+                System.out.println(0);
+                continue;
+             }
+
+             while(i>=0){
+                i-=(n-i-1);
+                ans++;
+
+                while(i>=0 && v[i]==v[n-1]){
+                    i--;
+                }
+             }
+             System.out.println(ans);
+
+        }
+
+
+    }
+}
