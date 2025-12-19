a2faa62fc45b526091814aa1cb6894cafa9afec1
Arya <arypatel913@gmail.com>
2025-12-19 22:20:11 +0530

    contest

---
+++ ashmal.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<string> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        string s = "";
+
+       for(int i=0;i<n;i++){
+         string front=a[i]+s;
+         string back=s+a[i];
+
+         if(back<front){
+            s=back;
+         }
+         else{
+            s=front;
+         }
+       }
+
+        cout << s << "\n";
+    }
+    return 0;
+}
