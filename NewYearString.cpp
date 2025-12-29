8234c39f3f4efb5a4c34db3861c69b98831f1948
Arya <arypatel913@gmail.com>
2025-12-29 20:43:49 +0530

    contest

---
+++ NewYearString.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+
+      
+        if(s.find("2025")==string::npos){
+            cout<<0<<endl;
+            continue;
+        }
+        //Need to create 2026
+        string target="2026";
+        int ans=INT_MAX;
+        for(int i=0;i<=n-4;i++){
+            int cost=0;//start new substring
+            for(int j=0;j<4;j++){
+                if(s[i+j]!=target[j]) cost++;
+            }
+            ans=min(ans,cost);
+        }
+        cout<<ans<<endl;
+        
+    }
+}
