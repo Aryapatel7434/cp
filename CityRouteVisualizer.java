84ccf8eb3e6b23ca1718c1e2f40d6475deb9188b
Arya <arypatel913@gmail.com>
2026-01-18 22:38:03 +0530

    contest

---
+++ CityRouteVisualizer.java
@@ -0,0 +1,23 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.util.List;
+import javax.swing.Timer;
+
+public class CityRouteVisualizer extends JFrame{
+    static class City{
+        String name;
+        int x,y;
+        City(String n,int x,int y){
+            name=n;
+            this.x=x;
+            this.y=y;
+        }
+
+    }
+    static class Edge{
+        
+    }
+}
