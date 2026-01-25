aba50b97fdec48631093ff5d21958e062a0c66dc
Arya <arypatel913@gmail.com>
2026-01-25 21:04:02 +0530

    change

--- replace sum.cpp
+++ replace sum.cpp
@@ -1,30 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
-    cin>>t;
-    while(t--){
-        int n,q;
-        cin>>n>>q;
+    cin >> t;
+    while (t--) {
+        int n, q;
+        cin >> n >> q;
 
-        vector<int>a(n+1),b(n+1);
+        vector<int> a(n+2), b(n+2);
+        for (int i = 1; i <= n; i++) cin >> a[i];
+        for (int i = 1; i <= n; i++) cin >> b[i];
 
-        for(int i=1;i<=n;i++) cin>>a[i];
-        for(int i=1;i<=n;i++) cin>>b[i];
-         //main focus aarray b compute prefix max of array b and compute the prefix sum of the best array and retun the preticulat query range prefix sum.
-        vector<int>best(n+2,0);
-        for(int i=n;i>=1;i--){
-            best[i]=max(b[i],best[i+1]);
+        vector<long long> best(n+2, 0);
+
+        for (int i = n; i >= 1; i--) {
+            best[i] = max({(long long)a[i], (long long)b[i], best[i+1]});
         }
-        vector<long long>pref(n+1,0);
-        for(int i=1;i<=n;i++){
-            pref[i]=pref[i-1]+best[i];
+
+        vector<long long> pref(n+1, 0);
+        for (int i = 1; i <= n; i++) {
+            pref[i] = pref[i-1] + best[i];
         }
-        while(q--){
-            int l,r;
-            cin>>l>>r;
-            cout<<pref[r]-pref[l-1]<<" ";
+
+        while (q--) {
+            int l, r;
+            cin >> l >> r;
+            cout << pref[r] - pref[l-1] << " ";
         }
-        cout<<endl;
+        cout << "\n";
     }
 }
