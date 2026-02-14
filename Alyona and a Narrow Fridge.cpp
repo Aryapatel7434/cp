3654d278cc1749d81eebea6da6788ac1b304770a
Arya <arypatel913@gmail.com>
2026-02-14 12:23:26 +0530

    1300

---
+++ Alyona and a Narrow Fridge.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,h;
+    cin>>n>>h;
+
+    vector<int>v(n);
+
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    //apply the logic of binary search
+    int low=0,high=n-1,ans=0;
+
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        vector<int>temp;
+
+        for(int i=0;i<=mid;i++){
+            temp.push_back(v[i]);
+        }
+        sort(temp.begin(),temp.end());
+
+        int height_required=0,p=mid;
+
+        while(p>=0){
+            height_required+=temp[p];
+            p-=2;
+
+            if(height_required>h){
+                break;
+            }
+        }
+        if(height_required>h){
+            high=mid-1;
+        }
+        else{
+            ans=mid;
+            low=mid+1;
+        }
+        
+    }
+    cout<<ans+1<<endl;
+}
