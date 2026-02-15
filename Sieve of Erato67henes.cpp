9425e8ab897760976545c1f4128b1779f0f3a771
Arya <arypatel913@gmail.com>
2026-02-15 20:19:52 +0530

    x

---
+++ Sieve of Erato67henes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++) cin>>a[i];
+        if (find(a.begin(), a.end(), 67) != a.end()){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
